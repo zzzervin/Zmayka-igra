477b33fabbf4bb6a22708e9106a260cb8322f229
Opilane TTHK <opilane@tthk.ee>
2021-02-15 14:29:34 +0200

    Add project files.

---
+++ Zmayka igra/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zmayka_igra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 1;
+            int y1=3;
+            char sum1 = '*';
+
+            Draw(x1, y1, sum1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sum2 = '#';
+
+            Draw(x2, y2, sum2);
+
+            Console.ReadLine();
+        }
+        
+        static void Draw(int x,int y,char sum)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sum);
+
+
+        }
+
+
+    }
+}
